9a7e0c71baa9f85cbf5e00f2ecf0d6037e0566a8
tMorriss <tmorriss@tmorriss.com>
2021-01-11 18:27:36 +0900

    add str() to db name

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -6,7 +6,7 @@ from .common import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
